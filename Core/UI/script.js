5f1d1a0ef0dff0d44e23da77df0a752672dd8f0a
TimDonnersPXL <65772844+Donrskbb@users.noreply.github.com>
2024-02-27 11:40:59 +0100

    Website functions

---
+++ Core/UI/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    setTimeout(function() {
+      document.querySelector('.loading-screen').style.display = 'none';
+    }, 300000);
+  });
+  
